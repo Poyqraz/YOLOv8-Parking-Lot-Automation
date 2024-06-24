30f22dcae52ec21dd45e1df4f81d78481de8095f
Poyraz BAYDEMİR <48729799+Poyqraz@users.noreply.github.com>
2024-06-24 23:05:46 +0300

    Delete py_ardu directory

--- py_ardu/led_yakma.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-ardu=serial.Serial('com3', 9600)
-while True:
-    islem=input("A=yak, E=sondur:")
-    if (islem=="a"):
-        ardu.write(b'a')
-    elif (islem=="e"):
-        ardu.write(b'e')
